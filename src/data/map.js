610d72b61920dbb97d79ed70c65e2849ffdff2e7
Andy G <agup2001@msn.com>
2020-06-10 08:29:35 -0600

    add CIM symbol to highlight selected trail

--- src/data/map.js
+++ src/data/map.js
@@ -1,12 +1,76 @@
-import { loadModules, loadCss } from 'esri-loader';
+import { loadModules, loadCss, setDefaultOptions } from 'esri-loader';
 
 const app = {};
+let sym2 = {}; 
+const sym1 = {
+  type: "cim",
+  // CIM Line Symbol
+  data: {
+    type: "CIMLineSymbol",
+    symbolLayers: [{
+      // white dashed layer at center of the line
+      type: "CIMSolidStroke",
+      effects: [{
+        type: "CIMGeometricEffectDashes",
+        dashTemplate: [2, 2, 2, 2],
+        lineDashEnding: "NoConstraint",
+        controlPointEnding: "NoConstraint"
+      }],
+      enable: "true",
+      capStyle: "Butt",
+      joinStyle: "Round",
+      width: 1,
+      color: [
+        255,
+        255,
+        255,
+        255
+      ]
+    },
+    {
+      // lighter green line layer that surrounds the dashes
+      type: "CIMSolidStroke",
+      enable: "true",
+      capStyle: "Butt",
+      joinStyle: "Round",
+      width: 3,
+      color: [
+        56,
+        168,
+        0,
+        255
+      ]
+    },
+    {
+      // darker green outline around the line symbol
+      type: "CIMSolidStroke",
+      enable: true,
+      capStyle: "Butt",
+      joinStyle: "Round",
+      width: 6,
+      color: [
+        0,
+        115,
+        76,
+        255
+      ]
+    }
+    ]
+  }
+}
 
 export async function initWebMap() {
-  const [ WebMap, ElevationLayer ] = await loadModules([
+  const [WebMap, ElevationLayer, SimpleLineSymbol ] = await loadModules([
     'esri/WebMap',
-    'esri/layers/ElevationLayer'
+    'esri/layers/ElevationLayer',
+    'esri/symbols/SimpleLineSymbol',
   ]);
+
+  sym2 = new SimpleLineSymbol({
+    color: "blue",
+    width: "1px",
+    style: "solid"
+  })  
 
   const webmap = new WebMap({
     portalItem: {
@@ -28,6 +92,7 @@ export async function initWebMap() {
 
 export async function initView(container) {
   loadCss();
+  setDefaultOptions({ version: 'next' })
   const [ MapView ] = await loadModules([
     'esri/views/MapView'
   ]);
@@ -56,6 +121,28 @@ export async function initView(container) {
   return view;
 }
 
+async function applyRenderer (exp, featureLayer) {
+  const [UniqueValueRenderer] = await loadModules([
+    'esri/renderers/UniqueValueRenderer'
+  ]);
+  console.log("sym2 ", sym2);
+  const renderer = new UniqueValueRenderer({
+    valueExpression: exp,
+    uniqueValueInfos: [{
+      value: 'true',
+      symbol: sym1,
+      label: "red"
+    },
+    {
+      value: 'false',
+      symbol: sym2,
+      label: "blue"
+    }]
+  });
+
+  featureLayer.renderer = renderer;
+}
+
 export async function filterMapData(name) {
   const [{ whenFalseOnce }] = await loadModules(['esri/core/watchUtils']);
   const layer = app.webmap.layers.getItemAt(0) // could be better
@@ -65,11 +152,32 @@ export async function filterMapData(name) {
   const query = layer.createQuery();
   query.where = `manager = '${name}'`;
   const extent = await layer.queryExtent(query);
+  const ids = await layer.queryObjectIds(query.clone());
+  console.log(ids);
+  const arcade = `
+     if(indexof([` + ids + `], $feature.FID) != -1){
+      // Apply a certain symbol
+      return true;
+    }
+    else {
+      // Or, apply a different symbol
+      return false;
+    } 
+  `
+  console.log(arcade);
+  applyRenderer(arcade, layer);
+  // layer.queryFeatures(query).then(function (results) {
+  //   console.log(results.features);
+  //   results.features.forEach( (e) => {
+  //     e.clone();
+  //     e.symbol = sym1;
+  //   });
+  // });  
   await app.view.goTo(extent);
   layerView.effect = {
     filter: {
       where: `manager = '${name}'`
     },
-    excludedEffect: 'grayscale(25%) opacity(45%)'
+    excludedEffect: 'grayscale(25%) opacity(35%)'
   };
 }
