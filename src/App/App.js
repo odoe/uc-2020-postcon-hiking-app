b685600ee01fa966b6a433e2bb1f9f709ba86c3e
Michael Gaigg <mgaigg@esri.com>
2020-07-03 13:40:58 -0700

    simplified app.js

--- src/App/App.js
+++ src/App/App.js
@@ -22,55 +22,23 @@ import { ToastContainer } from 'calcite-react/Toaster';
 const App = ({ user = {} }) => {
   const value = useSelected();
   const [userInfo] = useState(user);
-
-  // useEffect(() => {
-  //   setUserInfo(user);
-  // }, [user]);
-
-  const getPages = () => {
-    // return (
-    //   <Switch>
-    //     <Route exact path={Routes.Home}>
-    //       <HomePage user={user} />
-    //     </Route>
-    //     <Route path={Routes.Map}>
-    //       <SelectedContext.Provider value={value}>
-    //         <MapContext.Provider value={webmapId}>
-    //           <MapPage />
-    //         </MapContext.Provider>
-    //       </SelectedContext.Provider>
-    //     </Route>
-    //     <Route path="*">
-    //       <NoMatch />
-    //     </Route>
-    //   </Switch>
-    // );
-
-    return (
-      <Switch>
-        <Route exact path={Routes.Home}>
-          <HomePage user={user} />
-        </Route>
-        <Route path={Routes.Map}>
-          <SelectedContext.Provider value={value}>
-            <MapContextProvider>
-              <HomePage user={user} />
-              <MapPage />
-            </MapContextProvider>
-          </SelectedContext.Provider>
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    );
-  };
-
   return (
     <UserContext.Provider value={userInfo}>
       <StyledApp data-testid="App">
         <ToastContainer />
-        <div className="App">{getPages()}</div>
+        <Switch>
+          <Route path={Routes.Map}>
+            <SelectedContext.Provider value={value}>
+              <MapContextProvider>
+                <HomePage user={user} />
+                <MapPage />
+              </MapContextProvider>
+            </SelectedContext.Provider>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
       </StyledApp>
     </UserContext.Provider>
   );
