f04c0e45dfa0f5273542c72cfd6619c91251d43a
Joshua Peterson <jpeterson@esri.com>
2020-07-03 14:27:01 -0600

    wire up TrailSearch

--- src/components/TrailSearch/TrailSearchController.js
+++ src/components/TrailSearch/TrailSearchController.js
@@ -5,9 +5,11 @@ const init = async ({ view }) => {
     'esri/widgets/Search/SearchViewModel',
     'esri/tasks/Locator',
   ]);
+
   const vm = new SearchViewModel({
     view,
-    includeDefaultSources: false,
+    // TODO: I turned off "By Address" in the webmap settings, but World Geocoder is still in default sources...
+    includeDefaultSources: true,
     sources: [
       {
         locator: new Locator({
@@ -23,12 +25,50 @@ const init = async ({ view }) => {
       },
     ],
   });
+
   return vm;
 };
 
 const suggest = async ({ vm, value }) => {
+  // Use the SearchViewModel to get suggestions
   const suggestResponse = await vm.suggest(value);
-  return suggestResponse.results[0].results;
+
+  // Create a lookup for our sources
+  const sourceNameLookup = {};
+  vm.allSources.forEach((source, i) => {
+    sourceNameLookup[i] = abbrevSourceName[source.name];
+  });
+
+  // Flatten results into a single array
+  let allResults = [];
+  suggestResponse.results.forEach(({ results }) => {
+    allResults = [...allResults, ...results];
+  });
+
+  // Format results before returning to TrailSearch component
+  const formattedResults = allResults.map((suggestResult) => {
+    return {
+      text: suggestResult.text,
+      key: `${suggestResult.key}_${suggestResult.sourceIndex}`,
+      source: sourceNameLookup[suggestResult.sourceIndex],
+      suggestResult,
+    };
+  });
+
+  return formattedResults;
 };
 
-export { init, suggest };
+const search = async ({ vm, value }) => {
+  const searchResponse = await vm.search(value);
+  console.log(searchResponse);
+  return searchResponse;
+};
+
+const abbrevSourceName = {
+  'Trails: name': 'Trail',
+  'Trailheads: name': 'Trailhead',
+  'ArcGIS World Geocoding Service': 'Place',
+  'Colorado Search': 'Place',
+};
+
+export { init, suggest, search };
