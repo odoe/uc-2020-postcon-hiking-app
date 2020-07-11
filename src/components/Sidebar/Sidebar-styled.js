e983b6919a0a2642159339a7c23c0987e091e350
Michael Gaigg <mgaigg@esri.com>
2020-07-10 17:30:30 -0700

    modified cards layout, implemented react-window, added random images (#16)

--- src/components/Sidebar/Sidebar-styled.js
+++ src/components/Sidebar/Sidebar-styled.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 const StyledSidebar = styled.div`
-  display: flex;
-  flex-direction: column;
+  height: 100%;
   width: 100%;
 `;
 
--- src/components/TrailCard/TrailCard-styled.js
+++ src/components/TrailCard/TrailCard-styled.js
@@ -1,7 +1,72 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Card, { CardTitle, CardContent, CardImage } from 'calcite-react/Card';
 
-const StyledTrailCard = styled.div`
-  display: flex;
+const StyledCard = styled(Card)`
+  &:hover {
+    cursor: pointer;
+    background: ${({ theme }) => theme.palette.background};
+  }
 `;
 
-export { StyledTrailCard };
+const StyledCardTitle = styled(CardTitle)`
+  font-size: 1.2rem;
+`;
+
+const StyledCardContent = styled(CardContent)`
+  padding: 0.55rem 1rem;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0.5rem 0;
+`;
+
+const IconWrapper = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+  border-radius: 5px;
+  padding: 5px;
+  margin-inline-end: 0.3rem;
+  background: ${({ theme }) => theme.palette.semantic.inactive};
+
+  ${(props) =>
+    props.isActive &&
+    css`
+      background: ${({ theme }) => theme.palette.semantic.active};
+    `};
+
+  svg {
+    fill: ${({ theme }) => theme.palette.white};
+  }
+`;
+
+const StatsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const DistanceWrapper = styled.div`
+  svg {
+    color: #17231e38;
+  }
+`;
+
+const ElevationWrapper = styled.div`
+  svg {
+    color: #17231e38;
+  }
+`;
+
+export {
+  StyledCard,
+  StyledCardTitle,
+  StyledCardContent,
+  IconsWrapper,
+  IconWrapper,
+  StatsWrapper,
+  DistanceWrapper,
+  ElevationWrapper,
+};
